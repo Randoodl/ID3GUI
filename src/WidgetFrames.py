2350468b50227eb7915c070a75e96974df4cf100
Dylan <no@no.lmao>
2026-05-22 17:36:45 +0200

    Final touchups

--- src/WidgetFrames.py
+++ src/WidgetFrames.py
@@ -21,21 +21,21 @@ WidgetFrames = {
     "FoundFilesBox":       WidgetFrame(40,    9,      0,    2,   5,      "EW",     ""),
     "EditFilesLabel":      WidgetFrame(40,    1,      5,    1,   5,      "EW",     "Title Preview:"),
     "EditFilesBox":        WidgetFrame(40,    9,      5,    2,   5,      "EW",     ""),
-    "RenameLabel":         WidgetFrame(20,    1,      0,    3,   10,     "W",     " Isolating the title string from the file name:"),
+    "RenameLabel":         WidgetFrame(20,    1,      0,    3,   10,     "W",      " Isolating the title string from the file name:"),
     "CharIndexCutoffBox":  WidgetFrame(15,    1,      2,    4,   2,      "W",      ""),
     "CharIndexLimitBox":   WidgetFrame(15,    1,      5,    4,   2,      "W",      ""),
     "CharCutoffLabel":     WidgetFrame(16,    1,      0,    4,   2,      "W",      "   Leading characters: "),
     "CharLimitLabel":      WidgetFrame(16,    1,      4,    4,   1,      "W",      " Trailing characters: "),
     "TagLabel":            WidgetFrame(16,    1,      0,    5,   1,      "W",      " Tag setup:"),
-    "TPE1Label":           WidgetFrame(16,    1,      0,    6,   2,      "E",     " Artist:"),
+    "TPE1Label":           WidgetFrame(16,    1,      0,    6,   2,      "E",      " Artist:"),
     "TPE1Box":             WidgetFrame(46,    1,      2,    6,   5,      "EW",     ""),
-    "TALBLabel":           WidgetFrame(16,    1,      0,    7,   2,      "E",     " Album:"),
+    "TALBLabel":           WidgetFrame(16,    1,      0,    7,   2,      "E",      " Album:"),
     "TALBBox":             WidgetFrame(46,    1,      2,    7,   5,      "EW",     ""),
-    "TYERLabel":           WidgetFrame(16,    1,      0,    8,   2,      "E",     " Year:"),
+    "TYERLabel":           WidgetFrame(16,    1,      0,    8,   2,      "E",      " Year:"),
     "TYERBox":             WidgetFrame(46,    1,      2,    8,   5,      "EW",     ""),
-    "TCONLabel":           WidgetFrame(16,    1,      0,    9,   2,      "E",     " Genre:"),
+    "TCONLabel":           WidgetFrame(16,    1,      0,    9,   2,      "E",      " Genre:"),
     "TCONBox":             WidgetFrame(46,    1,      2,    9,   5,      "EW",     ""),
-    "MessageBox":          WidgetFrame(64,    1,      0,    10,   10,     "EW",     "")
+    "MessageBox":          WidgetFrame(64,    1,      0,    10,  10,     "EW",     "")
 }
 
 ListOfLabels      = ["FoundFilesLabel", "EditFilesLabel", "RenameLabel", "CharCutoffLabel", "CharLimitLabel", "TagLabel", "TPE1Label", "TALBLabel", "TYERLabel", "TCONLabel"]
